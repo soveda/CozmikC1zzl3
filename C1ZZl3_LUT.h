79bb3ca5263b408139618fd4613f11f0fb75aadf
soveda <161259864+soveda@users.noreply.github.com>
2026-06-08 09:00:21 +0100

    added info for LUT

---
+++ C1ZZl3_LUT.h
@@ -0,0 +1,16 @@
+//
+//  C1ZZl3_LUT.h
+//  
+//
+//  Created by Adrian Vos on 08/06/2026.
+//
+
+
+// C1ZZL3_LUT.h
+#pragma once
+
+#include <stdint.h>
+
+constexpr int SINE_SIZE = 1024;
+
+extern const int16_t sineLUT[SINE_SIZE];
